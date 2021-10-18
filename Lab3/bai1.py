22eaf3c08d66cf5d36f52585f0de3d1063fcec4d
Nhi <ynhi.hh80@gmail.com>
2021-10-18 21:46:59 +0700

    'upload'

--- Lab3/bai1.py
+++ Lab3/bai1.py
@@ -1,4 +1,4 @@
-filename = input('Which file would you like to back-up? ')
+filename = input('Which file would you like to back-up?')
 new_filename = filename + '.bak'
 backup = open(new_filename, 'w')
 for line in open(filename):
--- Lab3/bai7.py
+++ Lab3/bai7.py
@@ -1,9 +1,11 @@
 def read_molecule(reader):
     content = []
     for line in reader:
-        if not line.startswith('CMNT'):
+        if not line.startswith('radium'):
             if not line.isspace():
                 content.append(line.strip())
     for line in content:
         print(line)
-    
+input_file = open('D:\\Python\\PythonTH\\Lab3\\alkaline_metals.txt', 'r')
+print(read_molecule(input_file))
+input_file.close()
